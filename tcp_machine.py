d8b3db79df9c51b1d1a44dfcb81b8c06c445aed8
Your Name <you@example.com>
2019-10-29 17:06:35 -0700

    Removed unused states from TCP state machine.

--- tcp_machine.py
+++ tcp_machine.py
@@ -20,21 +20,15 @@ class TCPMachine(StateMachine):
     fin_wait_1 = State('FIN-WAIT-1')
     # FIN-WAIT-2: waiting for remote TCP termination request
     fin_wait_2 = State('FIN-WAIT-2')
-    # CLOSE-WAIT: waiting for termination request from local user
-    close_wait = State('CLOSE-WAIT')
-    # CLOSING: waiting for termination request from remote TCP
-    closing = State('CLOSING')
-    # LAST-ACK: waiting for ACK of previous termination request
-    last_ack = State('LAST-ACK')
     # TIME-WAIT: waiting for enough time to pass to be sure remote TCP received last ACK
     time_wait = State('TIME-WAIT')
 
     """ 3-WAY HANDSHAKE TRANSITIONS """
-    snd_syn = closed.to(syn_sent)  # Client socket is opened, sends SYN to server
-    recv_syn_ack = syn_sent.to(established)  # Client receives SYN-ACK, establishing connection
+    snd_syn = closed.to(syn_sent)  # Client socket is opened, send SYN to server
+    recv_syn_ack = syn_sent.to(established)  # Receive SYN-ACK + send ACK, establishing connection
 
     """ CONNECTION CLOSURE TRANSITIONS """
     snd_fin = established.to(fin_wait_1)  # On last data transmission, FIN=1
-    rcv_fin_ack = fin_wait_1.to(fin_wait_2)  # Remote TCP send segment with FIN=1 and ACK=1
-    snd_ack = fin_wait_1.to(closing)  # ACK remote TCP's FIN-ACK
-    timeout = time_wait.to(closed)  # Wait 2 MSL, then delete connection
+    rcv_ack = fin_wait_1.to(fin_wait_2)  # Receive ACK, send nothing
+    snd_ack = fin_wait_2.to(time_wait)  # Receive FIN, send ACK
+    timeout = time_wait.to(closed)  # Wait 30 seconds, then delete connection
