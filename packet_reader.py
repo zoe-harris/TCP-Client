f0d01f96d336e4a7e0a3f86260c9e36f86ec0b08
Your Name <you@example.com>
2019-11-06 14:12:00 -0900

    Split Segment class into Segment and PacketWriter. Segment class now creates a Segment object containing a packet and timer for that packet. PacketWriter handles writing of packet and setting of flags. PacketReader retrieves information from received packets.

---
+++ packet_reader.py
@@ -0,0 +1,22 @@
+# Zoe Harris
+# CSCE365 Computer Networks
+# Programming Assignment #2
+
+
+class PacketReader:
+
+    @staticmethod
+    def get_ack_num(segment):
+        return segment[64:96].uint
+
+    @staticmethod
+    def get_ack_bit(segment):
+        return segment[107]
+
+    @staticmethod
+    def get_syn_bit(segment):
+        return segment[110]
+
+    @staticmethod
+    def get_fin_bit(segment):
+        return segment[111]
